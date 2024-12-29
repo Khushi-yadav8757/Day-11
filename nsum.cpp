3421cf1f826ebc41ab608a2bd630a99fb48af61c
Khushi Yadav <kky957242@gmail.com>
2024-12-29 23:21:41 +0530

    Create nsum.cpp

---
+++ nsum.cpp
@@ -0,0 +1,14 @@
+// sum of n natural no.
+
+#include <iostream>
+using namespace std;
+int main(){
+int i,n ,sum=0;
+cout<<"enter the number:";
+cin>>n;
+for(i=1;i<=n;i++)
+{ 
+  sum=sum+i;
+}
+cout <<sum;
+}
